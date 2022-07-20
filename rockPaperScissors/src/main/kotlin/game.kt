36d882de70de4600261dcc46e8d1054d5f9b78a5
olisheikh <olisheikh707@gmail.com>
2022-07-20 14:14:02 +0600

    The End

--- rockPaperScissors/src/main/kotlin/game.kt
+++ rockPaperScissors/src/main/kotlin/game.kt
@@ -1,11 +1,45 @@
 fun main(){
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
-
-    options.forEach {print(it)}
+    val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
 }
 
-fun getGameChoice(optionParam: Array<String>): () -> Unit = {
-    //optionParam[(Math.random() * optionParam.size).toInt()]
-    optionParam[2] = "Knife"
+fun getGameChoice(optionParam: Array<String>) =
+    optionParam[(Math.random() * optionParam.size).toInt()]
+
+fun getUserChoice(optionParam: Array<String>): String{
+    var isValid = false
+    var userChoice = ""
+
+    while (!isValid){
+        println("Enter one of the following: ")
+        for (item in optionParam)
+            print(" $item")
+        print(".")
+
+        val userInput = readLine()!!
+        if (userInput != null){
+            isValid = true
+            userChoice = userInput
+        }else
+            print("Choose the valid option.")
+    }
+
+    return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String){
+    var result = ""
+
+    if (userChoice == gameChoice)
+        result = "tie"
+    else if (userChoice == "Paper" && gameChoice == "Rock" ||
+             userChoice == "Scissors" && gameChoice == "Paper" ||
+             userChoice == "Rock" && gameChoice == "Scissors")
+        result = "You win!"
+    else
+        result = "You lose!"
+
+    println("You choose $userChoice. I choose $gameChoice. $result")
 }
